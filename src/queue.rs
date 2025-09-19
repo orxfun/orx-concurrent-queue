3eff05a920019acec03fa10419d437e7ae6bfe12
orxfun <orx.ugur.arikan@gmail.com>
2025-09-19 19:19:12 +0200

    pop over the bag

--- src/queue.rs
+++ src/queue.rs
@@ -1,1 +1,25 @@
+use core::sync::atomic::{AtomicUsize, Ordering};
+use orx_concurrent_bag::{ConcurrentBag, Doubling, SplitVec};
 
+pub struct Queue<T> {
+    bag: ConcurrentBag<T, SplitVec<T, Doubling>>,
+    pushed: AtomicUsize,
+    popped: AtomicUsize,
+    len: AtomicUsize,
+}
+
+impl<T> Queue<T> {
+    pub fn pop(&self) -> Option<T> {
+        let prior = self.len.fetch_sub(1, Ordering::SeqCst);
+        match prior {
+            0 => {
+                self.len.store(0, Ordering::SeqCst);
+                None
+            }
+            _ => {
+                let idx = self.popped.fetch_add(1, Ordering::SeqCst);
+                Some(unsafe { self.bag.take(idx) })
+            }
+        }
+    }
+}
