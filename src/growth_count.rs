d5d3674506fa4b9f74b1ae58cc109c85d2170013
orxfun <orx.ugur.arikan@gmail.com>
2025-09-19 17:00:10 +0200

    mode growth handle

--- src/growth_count.rs
+++ src/growth_count.rs
@@ -15,4 +15,9 @@ impl GrowthCount {
         debug_assert_eq!(self.count.load(Ordering::Relaxed), SWITCHING);
         self.count.store(0, Ordering::Release);
     }
+
+    pub fn grew_once(&self) {
+        let before = self.count.fetch_sub(1, Ordering::Release);
+        debug_assert!(before > 0);
+    }
 }
--- src/handle_growth.rs
+++ src/handle_growth.rs
@@ -1,3 +1,17 @@
-// pub struct GrowthHandle<'a> {
-//     //
-// }
+use crate::growth_count::GrowthCount;
+
+pub struct HandleGrowth<'a> {
+    growth_count: &'a GrowthCount,
+}
+
+impl<'a> HandleGrowth<'a> {
+    pub fn new(growth_count: &'a GrowthCount) -> Self {
+        Self { growth_count }
+    }
+}
+
+impl Drop for HandleGrowth<'_> {
+    fn drop(&mut self) {
+        self.growth_count.grew_once();
+    }
+}
