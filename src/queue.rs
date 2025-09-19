dc4cb601d710c0f61136f087dc6f1c851159063a
orxfun <orx.ugur.arikan@gmail.com>
2025-09-19 22:02:10 +0200

    con_just_push

--- src/queue.rs
+++ src/queue.rs
@@ -1,8 +1,20 @@
 use std::mem::ManuallyDrop;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub struct Queue<T: Send> {
     capacity: usize,
     data: *mut T,
+    reserved: AtomicUsize,
+    pushed: AtomicUsize,
+    popped: AtomicUsize,
+}
+
+unsafe impl<T: Send> Sync for Queue<T> {}
+
+impl<T: Send> Drop for Queue<T> {
+    fn drop(&mut self) {
+        let _vec = unsafe { Vec::from_raw_parts(self.data, 0, self.capacity) };
+    }
 }
 
 impl<T: Send> Queue<T> {
@@ -10,12 +22,32 @@ impl<T: Send> Queue<T> {
         let mut v = Vec::with_capacity(capacity);
         let data = v.as_mut_ptr();
         let _ = ManuallyDrop::new(v);
-        Self { capacity, data }
+        Self {
+            capacity,
+            data,
+            reserved: 0.into(),
+            pushed: 0.into(),
+            popped: 0.into(),
+        }
     }
-}
 
-impl<T: Send> Drop for Queue<T> {
-    fn drop(&mut self) {
-        let _vec = unsafe { Vec::from_raw_parts(self.data, 0, self.capacity) };
+    unsafe fn ptr(&self, idx: usize) -> *mut T {
+        unsafe { self.data.add(idx) }
+    }
+
+    pub fn push(&self, value: T) {
+        let idx = self.reserved.fetch_add(1, Ordering::Acquire);
+        unsafe { self.ptr(idx).write(value) };
+        self.pushed.fetch_add(1, Ordering::Release);
+    }
+
+    pub fn as_slice(&mut self) -> &[T] {
+        let reserved = self.reserved.load(Ordering::Relaxed);
+        let pushed = self.pushed.load(Ordering::Relaxed);
+        let popped = self.popped.load(Ordering::Relaxed);
+        assert_eq!(reserved, pushed);
+        let begin = unsafe { self.ptr(popped) };
+        let len = pushed - popped;
+        unsafe { std::slice::from_raw_parts(begin, len) }
     }
 }
--- src/tests/con_push_pop.rs
+++ src/tests/con_push_pop.rs
@@ -1,6 +1,31 @@
 use crate::queue::Queue;
 
 #[test]
+fn con_just_push() {
+    let num_pushers = 4;
+    let num_ticks = 1000;
+
+    let capacity = num_pushers * num_ticks;
+
+    let mut queue = Queue::new(capacity);
+    let q = &queue;
+
+    std::thread::scope(|s| {
+        for t in 0..num_pushers {
+            s.spawn(move || {
+                for i in 0..num_ticks {
+                    q.push(t * num_ticks + i);
+                }
+            });
+        }
+    });
+
+    let mut pushed: Vec<_> = queue.as_slice().iter().copied().collect();
+    pushed.sort();
+    assert_eq!(pushed, (0..pushed.len()).collect::<Vec<_>>());
+}
+
+#[test]
 fn con_push_pop() {
     let queue = Queue::<usize>::new(100);
 }
