aad1662542d6cd20692979790a4ef58cab68f694
orxfun <orx.ugur.arikan@gmail.com>
2025-09-19 17:20:20 +0200

    impl get_growth_handle

--- src/growth_count.rs
+++ src/growth_count.rs
@@ -1,14 +1,18 @@
-use crate::handle_switch::HandleSwitch;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use crate::{handle_growth::HandleGrowth, handle_switch::HandleSwitch};
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 
 const SWITCHING: usize = usize::MAX;
 
 pub struct GrowthCount {
+    switch_requested: AtomicBool,
     count: AtomicUsize,
 }
 
 impl GrowthCount {
     pub fn get_switch_handle(&self) -> HandleSwitch<'_> {
+        let prior = self.switch_requested.fetch_or(true, Ordering::SeqCst);
+        debug_assert!(!prior);
+
         loop {
             match self
                 .count
@@ -21,13 +25,24 @@ impl GrowthCount {
         }
     }
 
+    pub fn get_growth_handle(&self) -> HandleGrowth<'_> {
+        loop {
+            if self.switch_requested.load(Ordering::Relaxed) {}
+            _ = self.count.fetch_add(1, Ordering::Release);
+            return HandleGrowth::new(self);
+        }
+    }
+
     pub fn switched(&self) {
         debug_assert_eq!(self.count.load(Ordering::Relaxed), SWITCHING);
         self.count.store(0, Ordering::Release);
     }
 
     pub fn grew_once(&self) {
-        let before = self.count.fetch_sub(1, Ordering::Release);
-        debug_assert!(before > 0);
+        let prior = self.switch_requested.fetch_and(false, Ordering::SeqCst);
+        debug_assert!(prior);
+
+        let prior = self.count.fetch_sub(1, Ordering::Release);
+        debug_assert!(prior > 0);
     }
 }
