583dd2976bf090021c805a3f327d662fb43539b5
orxfun <orx.ugur.arikan@gmail.com>
2025-09-19 22:14:59 +0200

    con_just_pop test added

--- src/queue.rs
+++ src/queue.rs
@@ -4,9 +4,9 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 pub struct Queue<T: Send> {
     capacity: usize,
     data: *mut T,
-    reserved: AtomicUsize,
+    push_reserved: AtomicUsize,
     pushed: AtomicUsize,
-    popped: AtomicUsize,
+    pop_reserved: AtomicUsize,
 }
 
 unsafe impl<T: Send> Sync for Queue<T> {}
@@ -25,10 +25,21 @@ impl<T: Send> Queue<T> {
         Self {
             capacity,
             data,
-            reserved: 0.into(),
+            push_reserved: 0.into(),
             pushed: 0.into(),
-            popped: 0.into(),
+            pop_reserved: 0.into(),
         }
+    }
+
+    pub fn as_slice(&mut self) -> &[T] {
+        let reserved = self.push_reserved.load(Ordering::Relaxed);
+        let pushed = self.pushed.load(Ordering::Relaxed);
+        assert_eq!(reserved, pushed);
+        let popped = self.pop_reserved.load(Ordering::Relaxed);
+
+        let begin = unsafe { self.ptr(popped) };
+        let len = pushed - popped;
+        unsafe { std::slice::from_raw_parts(begin, len) }
     }
 
     unsafe fn ptr(&self, idx: usize) -> *mut T {
@@ -36,18 +47,15 @@ impl<T: Send> Queue<T> {
     }
 
     pub fn push(&self, value: T) {
-        let idx = self.reserved.fetch_add(1, Ordering::Acquire);
+        let idx = self.push_reserved.fetch_add(1, Ordering::Acquire);
         unsafe { self.ptr(idx).write(value) };
         self.pushed.fetch_add(1, Ordering::Release);
     }
 
-    pub fn as_slice(&mut self) -> &[T] {
-        let reserved = self.reserved.load(Ordering::Relaxed);
-        let pushed = self.pushed.load(Ordering::Relaxed);
-        let popped = self.popped.load(Ordering::Relaxed);
-        assert_eq!(reserved, pushed);
-        let begin = unsafe { self.ptr(popped) };
-        let len = pushed - popped;
-        unsafe { std::slice::from_raw_parts(begin, len) }
+    pub fn pop(&self) -> Option<T> {
+        let idx = self.pop_reserved.fetch_add(1, Ordering::Acquire);
+        while self.pushed.load(Ordering::Relaxed) <= idx {}
+        let value = unsafe { self.ptr(idx).read() };
+        Some(value)
     }
 }
--- src/tests/con_push_pop.rs
+++ src/tests/con_push_pop.rs
@@ -1,4 +1,5 @@
 use crate::queue::Queue;
+use orx_concurrent_bag::*;
 
 #[test]
 fn con_just_push() {
@@ -23,6 +24,39 @@ fn con_just_push() {
     let mut pushed: Vec<_> = queue.as_slice().iter().copied().collect();
     pushed.sort();
     assert_eq!(pushed, (0..pushed.len()).collect::<Vec<_>>());
+}
+
+#[test]
+fn con_just_pop() {
+    let num_poppers = 4;
+    let num_ticks = 1000;
+
+    let capacity = num_poppers * num_ticks;
+
+    let queue = Queue::new(capacity);
+    let q = &queue;
+    let collected = ConcurrentBag::new();
+    let bag = &collected;
+
+    for i in 0..capacity {
+        queue.push(i);
+    }
+
+    std::thread::scope(|s| {
+        for t in 0..num_poppers {
+            s.spawn(move || {
+                for _ in 0..num_ticks {
+                    if let Some(value) = q.pop() {
+                        bag.push(value);
+                    }
+                }
+            });
+        }
+    });
+
+    let mut collected = collected.into_inner();
+    collected.sort();
+    assert_eq!(collected, (0..collected.len()).collect::<Vec<_>>());
 }
 
 #[test]
