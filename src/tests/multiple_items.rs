5fbb51bbf44a570e4436c2c19a0ec07a4b2b6223
orxfun <orx.ugur.arikan@gmail.com>
2025-09-20 19:21:20 +0200

    con_extend_and_pull test passes

--- src/tests/multiple_items.rs
+++ src/tests/multiple_items.rs
@@ -78,3 +78,47 @@ fn con_just_pull() {
     collected.sort();
     assert_eq!(collected, expected);
 }
+
+#[test]
+fn con_extend_and_pull() {
+    let num_extenders = 4;
+    let num_pullers = 4;
+    let num_ticks = 1000;
+    let chunk_size = 17;
+
+    let capacity = num_extenders * num_ticks * 3;
+
+    let queue = Queue::new(capacity);
+    let collected = ConcurrentBag::new();
+
+    let q = &queue;
+    std::thread::scope(|s| {
+        for t in 0..num_extenders {
+            s.spawn(move || {
+                for i in 0..num_ticks {
+                    let value = t * num_ticks + i;
+                    let items = [value, 1_000_000 + value, 10_000_000 + value]
+                        .into_iter()
+                        .map(|x| x + 1);
+                    match t.is_multiple_of(2) {
+                        true => q.extend(items),
+                        false => unsafe { q.extend_n_items(items, 3) },
+                    }
+                }
+            });
+        }
+    });
+
+    let q = &queue;
+    std::thread::scope(|s| {
+        for _ in 0..num_pullers {
+            s.spawn(|| {
+                for _ in 0..(num_ticks + 20) {
+                    if let Some(values) = q.pull(chunk_size) {
+                        collected.extend(values);
+                    }
+                }
+            });
+        }
+    });
+}
