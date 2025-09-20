4688f33eb019c19b8daa6a3ff9df9e7e972c7308
orxfun <orx.ugur.arikan@gmail.com>
2025-09-20 21:49:53 +0200

    concurrent queue is defined as a wrapper around a concurrent-pinned-vec

--- src/queue.rs
+++ src/queue.rs
@@ -1,10 +1,46 @@
 use crate::state::ConcurrentQueueState;
-use orx_pinned_concurrent_col::{PinnedConcurrentCol, prelude::IntoConcurrentPinnedVec};
+use orx_pinned_vec::ConcurrentPinnedVec;
+use std::{marker::PhantomData, mem::ManuallyDrop, sync::atomic::Ordering};
 
 pub struct ConcurrentQueue<T, P>
 where
     T: Send,
-    P: IntoConcurrentPinnedVec<T>,
+    P: ConcurrentPinnedVec<T>,
 {
-    core: PinnedConcurrentCol<T, P::ConPinnedVec, ConcurrentQueueState>,
+    phantom: PhantomData<T>,
+    vec: P,
+    state: ConcurrentQueueState,
+}
+
+unsafe impl<T, P> Sync for ConcurrentQueue<T, P>
+where
+    T: Send,
+    P: ConcurrentPinnedVec<T>,
+{
+}
+
+impl<T, P> Drop for ConcurrentQueue<T, P>
+where
+    T: Send,
+    P: ConcurrentPinnedVec<T>,
+{
+    fn drop(&mut self) {
+        if core::mem::needs_drop::<T>() {
+            let s = &self.state;
+            let popped = s.popped.load(Ordering::Relaxed);
+            let pushed = s.pushed.load(Ordering::Relaxed);
+            for i in popped..pushed {
+                let ptr = unsafe { self.vec.get_ptr_mut(i) };
+                unsafe { ptr.drop_in_place() };
+            }
+        }
+        // let _vec = unsafe { Vec::from_raw_parts(self.data, 0, self.capacity) };
+    }
+}
+
+impl<T, P> ConcurrentQueue<T, P>
+where
+    T: Send,
+    P: ConcurrentPinnedVec<T>,
+{
 }
--- src/state.rs
+++ src/state.rs
@@ -9,71 +9,7 @@ use orx_pinned_concurrent_col::{
 };
 
 pub struct ConcurrentQueueState {
-    len: AtomicIsize,    // written_len
-    pushed: AtomicUsize, // len
-    popped: AtomicUsize,
-}
-
-impl<T> ConcurrentState<T> for ConcurrentQueueState {
-    fn fill_memory_with(&self) -> Option<fn() -> T> {
-        None
-    }
-
-    fn new_for_pinned_vec<P: PinnedVec<T>>(pinned_vec: &P) -> Self {
-        Self {
-            len: (pinned_vec.len() as isize).into(),
-            pushed: pinned_vec.len().into(),
-            popped: 0.into(),
-        }
-    }
-
-    fn new_for_con_pinned_vec<P: ConcurrentPinnedVec<T>>(_: &P, len: usize) -> Self {
-        Self {
-            len: (len as isize).into(),
-            pushed: len.into(),
-            popped: 0.into(),
-        }
-    }
-
-    fn write_permit<P>(&self, col: &PinnedConcurrentCol<T, P, Self>, idx: usize) -> WritePermit
-    where
-        P: ConcurrentPinnedVec<T>,
-    {
-        let capacity = col.capacity();
-
-        match idx.cmp(&capacity) {
-            Ordering::Less => WritePermit::JustWrite,
-            Ordering::Equal => WritePermit::GrowThenWrite,
-            Ordering::Greater => WritePermit::Spin,
-        }
-    }
-
-    fn write_permit_n_items<P>(
-        &self,
-        col: &PinnedConcurrentCol<T, P, Self>,
-        begin_idx: usize,
-        num_items: usize,
-    ) -> WritePermit
-    where
-        P: ConcurrentPinnedVec<T>,
-    {
-        let capacity = col.capacity();
-        let last_idx = begin_idx + num_items - 1;
-
-        match (begin_idx.cmp(&capacity), last_idx.cmp(&capacity)) {
-            (_, core::cmp::Ordering::Less) => WritePermit::JustWrite,
-            (core::cmp::Ordering::Greater, _) => WritePermit::Spin,
-            _ => WritePermit::GrowThenWrite,
-        }
-    }
-
-    fn release_growth_handle(&self) {}
-
-    fn update_after_write(&self, _: usize, end_idx: usize) {
-        self.len.store(end_idx as isize, atomic::Ordering::Release);
-    }
-
-    fn try_get_no_gap_len(&self) -> Option<usize> {
-        Some(self.pushed.load(atomic::Ordering::Acquire))
-    }
+    pub(super) len: AtomicIsize,    // written_len
+    pub(super) pushed: AtomicUsize, // len
+    pub(super) popped: AtomicUsize,
 }
