c9b3f24e743f0741d19e2d79060f1e2773a7f567
orxfun <orx.ugur.arikan@gmail.com>
2025-09-21 20:20:51 +0200

    impl default for queue

--- src/new.rs
+++ src/new.rs
@@ -5,6 +5,15 @@ use orx_split_vec::{Doubling, SplitVec};
 type DefaultPinnedVec<T> = SplitVec<T, Doubling>;
 pub type DefaultConVec<T> = <DefaultPinnedVec<T> as IntoConcurrentPinnedVec<T>>::ConPinnedVec;
 
+impl<T> Default for ConcurrentQueue<T, DefaultConVec<T>>
+where
+    T: Send,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl<T> ConcurrentQueue<T, DefaultConVec<T>>
 where
     T: Send,
