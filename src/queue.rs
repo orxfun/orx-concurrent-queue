981e0874d6fdc09e27163ab6a21424da01f24a89
orxfun <orx.ugur.arikan@gmail.com>
2025-09-26 17:52:06 +0200

    queue is documented with doc tests

--- src/queue.rs
+++ src/queue.rs
@@ -41,6 +41,87 @@ where
 /// [`pull`]: crate::ConcurrentQueue::pull
 ///
 /// # Examples
+///
+/// The following example demonstrates a basic usage of the queue within a synchronous program.
+/// Note that push, extend, pop and pull methods can be called with a shared reference `&self`.
+/// This will provide the convenience to use the queue in concurrent program as will be demonstrated in the second example.
+///
+/// ```
+/// use orx_concurrent_queue::ConcurrentQueue;
+///
+/// let queue = ConcurrentQueue::new();
+///
+/// queue.push(0); // [0]
+/// queue.push(1); // [0, 1]
+///
+/// let x = queue.pop(); // [1]
+/// assert_eq!(x, Some(0));
+///
+/// queue.extend(2..7); // [1, 2, 3, 4, 5, 6]
+///
+/// let x: Vec<_> = queue.pull(4).unwrap().collect(); // [5, 6]
+/// assert_eq!(x, vec![1, 2, 3, 4]);
+///
+/// assert_eq!(queue.len(), 2);
+///
+/// let vec = queue.into_inner();
+/// assert_eq!(vec, vec![5, 6]);
+/// ```
+///
+/// In the following example, the queue is created with three pre-populated tasks.
+/// Every task might potentially lead to new task or tasks.
+/// New tasks are added to the back of the queue, to be popped later and potentially add new tasks to the queue.
+///
+/// ```
+/// use orx_concurrent_queue::ConcurrentQueue;
+/// use std::sync::atomic::{AtomicUsize, Ordering};
+///
+/// struct Task {
+///     micros: usize,
+/// }
+///
+/// impl Task {
+///     fn perform(&self) {
+///         std::thread::sleep(std::time::Duration::from_micros(self.micros as u64));
+///     }
+///
+///     fn child_tasks(&self) -> impl ExactSizeIterator<Item = Task> {
+///         let range = match self.micros < 5 {
+///             true => 0..0,
+///             false => 0..self.micros,
+///         };
+///
+///         range.rev().take(5).map(|micros| Self { micros })
+///     }
+/// }
+///
+/// let queue = ConcurrentQueue::new();
+/// for micros in [10, 15, 10] {
+///     queue.push(Task { micros });
+/// }
+///
+/// let num_performed_tasks = AtomicUsize::new(queue.len());
+///
+/// let num_threads = 8;
+/// std::thread::scope(|s| {
+///     for _ in 0..num_threads {
+///         s.spawn(|| {
+///             // pop a task from front of the queue, as long as there are more tasks
+///             while let Some(task) = queue.pop() {
+///                 // create children tasks, add to back of the same queue
+///                 queue.extend(task.child_tasks());
+///
+///                 // perform the popped task
+///                 task.perform();
+///
+///                 _ = num_performed_tasks.fetch_add(1, Ordering::Relaxed);
+///             }
+///         });
+///     }
+/// });
+///
+/// assert_eq!(num_performed_tasks.load(Ordering::Relaxed), 5046);
+/// ```
 pub struct ConcurrentQueue<T, P = DefaultConVec<T>>
 where
     T: Send,
@@ -293,10 +374,61 @@ where
 
 #[cfg(test)]
 mod tsts {
+    use std::dbg;
+
     use super::*;
     use alloc::vec;
     use alloc::vec::Vec;
     // use crate::*;
+
+    struct Task {
+        micros: usize,
+    }
+
+    impl Task {
+        fn perform(&self) {
+            std::thread::sleep(std::time::Duration::from_micros(self.micros as u64));
+        }
+
+        fn child_tasks(&self) -> impl ExactSizeIterator<Item = Task> {
+            let range = match self.micros < 5 {
+                true => 0..0,
+                false => 0..self.micros,
+            };
+
+            range.rev().take(5).map(|micros| Self { micros })
+        }
+    }
+
+    #[test]
+    fn xyz() {
+        let queue = ConcurrentQueue::new();
+        for micros in [10, 15, 10] {
+            queue.push(Task { micros });
+        }
+
+        let num_performed_tasks = AtomicUsize::new(queue.len());
+
+        let num_threads = 8;
+        std::thread::scope(|s| {
+            for _ in 0..num_threads {
+                s.spawn(|| {
+                    // pop a task from front of the queue, as long as there are more tasks
+                    while let Some(task) = queue.pop() {
+                        // create children tasks, add to back of the same queue
+                        queue.extend(task.child_tasks());
+
+                        // perform the popped task
+                        task.perform();
+
+                        _ = num_performed_tasks.fetch_add(1, Ordering::Relaxed);
+                    }
+                });
+            }
+        });
+
+        assert_eq!(num_performed_tasks.load(Ordering::Relaxed), 5046);
+    }
 
     #[test]
     fn abc() {
@@ -308,11 +440,14 @@ mod tsts {
         let x = queue.pop(); // [1]
         assert_eq!(x, Some(0));
 
-        queue.extend(2..5); // [1, 2, 3, 4]
+        queue.extend(2..7); // [1, 2, 3, 4, 5, 6]
 
-        let x: Vec<_> = queue.pull(3).unwrap().collect(); // [4]
-        assert_eq!(x, vec![1, 2, 3]);
+        let x: Vec<_> = queue.pull(4).unwrap().collect(); // [5, 6]
+        assert_eq!(x, vec![1, 2, 3, 4]);
 
-        assert_eq!(queue.len(), 1);
+        assert_eq!(queue.len(), 2);
+
+        let vec = queue.into_inner();
+        assert_eq!(vec, vec![5, 6]);
     }
 }
