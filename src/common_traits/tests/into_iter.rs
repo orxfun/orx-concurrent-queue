da43120b9cc19439638dabb98dae09a1934f7517
orxfun <orx.ugur.arikan@gmail.com>
2025-09-27 22:52:46 +0200

    into iter tests are extended by usage patterns

--- src/common_traits/tests/into_iter.rs
+++ src/common_traits/tests/into_iter.rs
@@ -14,9 +14,20 @@ fn into_iter_empty<P>(vec: P)
 where
     P: IntoConcurrentPinnedVec<String> + Clone,
 {
-    let queue = ConcurrentQueue::from(vec);
-    let iter = queue.into_iter();
-    assert_eq!(iter.count(), 0);
+    let iter = || {
+        let queue = ConcurrentQueue::from(vec.clone());
+        queue.into_iter()
+    };
+
+    let consume_all = iter().count();
+    assert_eq!(consume_all, 0);
+
+    let mut consume_half = iter();
+    for _ in 0..10 {
+        _ = consume_half.next();
+    }
+
+    let _consume_none = iter();
 }
 
 #[test_matrix([
@@ -28,13 +39,54 @@ fn into_iter_none_used<P>(vec: P)
 where
     P: IntoConcurrentPinnedVec<String> + Clone,
 {
-    let queue = ConcurrentQueue::from(vec);
-    for i in 0..20 {
-        queue.push(i.to_string());
+    let iter = || {
+        let queue = ConcurrentQueue::from(vec.clone());
+        for i in 0..20 {
+            queue.push(i.to_string());
+        }
+        queue.into_iter()
+    };
+
+    let consume_all = iter().count();
+    assert_eq!(consume_all, 20);
+
+    let mut consume_half = iter();
+    for _ in 0..10 {
+        _ = consume_half.next();
     }
 
-    let iter = queue.into_iter();
-    assert_eq!(iter.count(), 20);
+    let _consume_none = iter();
+}
+
+#[test_matrix([
+    FixedVec::new(30),
+    SplitVec::with_doubling_growth_and_max_concurrent_capacity(),
+    SplitVec::with_linear_growth_and_fragments_capacity(2, 64),
+])]
+fn into_iter_half_used<P>(vec: P)
+where
+    P: IntoConcurrentPinnedVec<String> + Clone,
+{
+    let iter = || {
+        let queue = ConcurrentQueue::from(vec.clone());
+        for i in 0..20 {
+            queue.push(i.to_string());
+        }
+        for _ in 0..7 {
+            _ = queue.pop();
+        }
+        queue.into_iter()
+    };
+
+    let consume_all = iter().count();
+    assert_eq!(consume_all, 13);
+
+    let mut consume_half = iter();
+    for _ in 0..10 {
+        _ = consume_half.next();
+    }
+
+    let _consume_none = iter();
 }
 
 #[test_matrix([
@@ -46,12 +98,22 @@ fn into_iter_fully_used<P>(vec: P)
 where
     P: IntoConcurrentPinnedVec<String> + Clone,
 {
-    let queue = ConcurrentQueue::from(vec);
-    for i in 0..20 {
-        queue.push(i.to_string());
-    }
-    while let Some(_) = queue.pop() {}
+    let iter = || {
+        let queue = ConcurrentQueue::from(vec.clone());
+        for i in 0..20 {
+            queue.push(i.to_string());
+        }
+        while let Some(_) = queue.pop() {}
+        queue.into_iter()
+    };
 
-    let iter = queue.into_iter();
-    assert_eq!(iter.count(), 0);
+    let consume_all = iter().count();
+    assert_eq!(consume_all, 0);
+
+    let mut consume_half = iter();
+    for _ in 0..10 {
+        _ = consume_half.next();
+    }
+
+    let _consume_none = iter();
 }
