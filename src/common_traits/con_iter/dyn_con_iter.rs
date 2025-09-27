52675a80d6b5984ffcd4d5d5644ffddeb5708f59
orxfun <orx.ugur.arikan@gmail.com>
2025-09-27 23:14:27 +0200

    chunk puller is wired to dynamic con iter

--- src/common_traits/con_iter/dyn_con_iter.rs
+++ src/common_traits/con_iter/dyn_con_iter.rs
@@ -1,4 +1,6 @@
-use crate::{ConcurrentQueue, queue::DefaultConVec};
+use crate::{
+    ConcurrentQueue, common_traits::con_iter::chunk_puller::DynChunkPuller, queue::DefaultConVec,
+};
 use orx_concurrent_iter::ConcurrentIter;
 use orx_pinned_vec::ConcurrentPinnedVec;
 
@@ -7,48 +9,51 @@ where
     T: Send,
     E: Fn(&T) -> I + Sync,
     I: IntoIterator<Item = T>,
+    I::IntoIter: ExactSizeIterator,
     P: ConcurrentPinnedVec<T>,
 {
     queue: ConcurrentQueue<T, P>,
     extend: E,
 }
 
-// impl<T, E, I, P> ConcurrentIter for DynamicConcurrentIter<T, E, I, P>
-// where
-//     T: Send,
-//     E: Fn(&T) -> I + Sync,
-//     I: IntoIterator<Item = T>,
-//     P: ConcurrentPinnedVec<T>,
-// {
-//     type Item = T;
+impl<T, E, I, P> ConcurrentIter for DynamicConcurrentIter<T, E, I, P>
+where
+    T: Send,
+    E: Fn(&T) -> I + Sync,
+    I: IntoIterator<Item = T>,
+    I::IntoIter: ExactSizeIterator,
+    P: ConcurrentPinnedVec<T>,
+{
+    type Item = T;
 
-//     type SequentialIter = core::iter::Empty<T>;
+    type SequentialIter = core::iter::Empty<T>;
 
-//     type ChunkPuller<'i>
-//     where
-//         Self: 'i;
+    type ChunkPuller<'i>
+        = DynChunkPuller<'i, T, E, I, P>
+    where
+        Self: 'i;
 
-//     fn into_seq_iter(self) -> Self::SequentialIter {
-//         todo!()
-//     }
+    fn into_seq_iter(self) -> Self::SequentialIter {
+        todo!()
+    }
 
-//     fn skip_to_end(&self) {
-//         todo!()
-//     }
+    fn skip_to_end(&self) {
+        todo!()
+    }
 
-//     fn next(&self) -> Option<Self::Item> {
-//         todo!()
-//     }
+    fn next(&self) -> Option<Self::Item> {
+        todo!()
+    }
 
-//     fn next_with_idx(&self) -> Option<(usize, Self::Item)> {
-//         todo!()
-//     }
+    fn next_with_idx(&self) -> Option<(usize, Self::Item)> {
+        todo!()
+    }
 
-//     fn size_hint(&self) -> (usize, Option<usize>) {
-//         todo!()
-//     }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        todo!()
+    }
 
-//     fn chunk_puller(&self, chunk_size: usize) -> Self::ChunkPuller<'_> {
-//         todo!()
-//     }
-// }
+    fn chunk_puller(&self, chunk_size: usize) -> Self::ChunkPuller<'_> {
+        DynChunkPuller::new(&self.extend, &self.queue, chunk_size)
+    }
+}
