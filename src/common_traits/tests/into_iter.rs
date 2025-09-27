75f02b4801a7b2cec79a5ce17197e662625e04ee
orxfun <orx.ugur.arikan@gmail.com>
2025-09-27 22:43:31 +0200

    into_iter_empty test

--- src/common_traits/tests/into_iter.rs
+++ src/common_traits/tests/into_iter.rs
@@ -1,9 +1,10 @@
 use crate::ConcurrentQueue;
-use std::string::String;
+use std::string::{String, ToString};
 
 #[test]
-fn empty() {
+fn into_iter_empty() {
     let queue = ConcurrentQueue::<String>::new();
+    queue.push("a".to_string());
     let iter = queue.into_iter();
-    assert_eq!(iter.count(), 0);
+    assert_eq!(iter.count(), 1);
 }
