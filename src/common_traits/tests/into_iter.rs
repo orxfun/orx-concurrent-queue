15fa674c2e51cab231dabf9487374eeb565141db
orxfun <orx.ugur.arikan@gmail.com>
2025-09-27 22:49:25 +0200

    impl into_iter_none_used tests

--- src/common_traits/tests/into_iter.rs
+++ src/common_traits/tests/into_iter.rs
@@ -12,7 +12,7 @@ use test_case::test_matrix;
 ])]
 fn into_iter_empty<P>(vec: P)
 where
-    P: IntoConcurrentPinnedVec<String>,
+    P: IntoConcurrentPinnedVec<String> + Clone,
 {
     let queue = ConcurrentQueue::from(vec);
     let iter = queue.into_iter();
@@ -24,9 +24,27 @@ where
     SplitVec::with_doubling_growth_and_max_concurrent_capacity(),
     SplitVec::with_linear_growth_and_fragments_capacity(2, 64),
 ])]
+fn into_iter_none_used<P>(vec: P)
+where
+    P: IntoConcurrentPinnedVec<String> + Clone,
+{
+    let queue = ConcurrentQueue::from(vec);
+    for i in 0..20 {
+        queue.push(i.to_string());
+    }
+
+    let iter = queue.into_iter();
+    assert_eq!(iter.count(), 20);
+}
+
+#[test_matrix([
+    FixedVec::new(30),
+    SplitVec::with_doubling_growth_and_max_concurrent_capacity(),
+    SplitVec::with_linear_growth_and_fragments_capacity(2, 64),
+])]
 fn into_iter_fully_used<P>(vec: P)
 where
-    P: IntoConcurrentPinnedVec<String>,
+    P: IntoConcurrentPinnedVec<String> + Clone,
 {
     let queue = ConcurrentQueue::from(vec);
     for i in 0..20 {
