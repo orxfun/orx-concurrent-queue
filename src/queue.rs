9c9ec530474ccfdbec7d1a9fb59b4af37606bdac
orxfun <orx.ugur.arikan@gmail.com>
2025-10-17 10:15:21 +0200

    implement num_written

--- src/queue.rs
+++ src/queue.rs
@@ -714,6 +714,56 @@ where
             .saturating_sub(self.popped.load(Ordering::Relaxed))
     }
 
+    /// Returns the total number of positions written; i.e., total of
+    /// number of times we pushed and sum of lengths of iterators that
+    /// we extended the queue with.
+    ///
+    /// See [`num_write_reserved`] to get the number of positions which are
+    /// reserved to be written.
+    ///
+    /// Note that in a synchronous program, number of reserved positions
+    /// will be equal to the number of written positions.
+    ///
+    /// In a concurrent program; however, it is possible to observe that
+    /// `num_write_reserved >= num_written` since we might observe the
+    /// counts while writing of some elements are in progress.
+    ///
+    /// However, we can never observe `num_write_reserved < num_written`.
+    ///
+    /// [`num_written`]: ConcurrentQueue::num_written
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_concurrent_queue::*;
+    /// use std::sync::atomic::Ordering;
+    ///
+    /// let queue = ConcurrentQueue::new();
+    ///
+    /// assert_eq!(queue.num_written(Ordering::Relaxed), 0);
+    ///
+    /// queue.push(1);
+    /// assert_eq!(queue.num_written(Ordering::Relaxed), 1);
+    ///
+    /// queue.extend([2, 3, 4]);
+    /// assert_eq!(queue.num_written(Ordering::Relaxed), 4);
+    ///
+    /// _ = queue.pop();
+    /// assert_eq!(queue.num_written(Ordering::Relaxed), 4);
+    ///
+    /// _ = queue.pull(2);
+    /// assert_eq!(queue.num_written(Ordering::Relaxed), 4);
+    ///
+    /// _ = queue.pull(10); // only 1 is pulled
+    /// assert_eq!(queue.num_written(Ordering::Relaxed), 4);
+    ///
+    /// _ = queue.pop(); // None
+    /// assert_eq!(queue.num_written(Ordering::Relaxed), 4);
+    /// ```
+    pub fn num_written(&self, order: Ordering) -> usize {
+        self.written.load(order)
+    }
+
     /// Returns the total number of positions reserved to be written.
     ///
     /// See [`num_written`] to get the number of elements which are
